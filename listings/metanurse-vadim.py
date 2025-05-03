4b4ce4dbcccf2a2afb1861c977e186caed7bf1a9
Vadim Liventsev <dev@vadim.me>
2025-05-03 11:47:46 +0200

    unified metanurse code

--- listings/metanurse-vadim.py
+++ listings/metanurse-vadim.py
@@ -1,15 +1,18 @@
-do_nothing = 0
+just_work = 0
 drink_coffee = 1
 drink_beer = 2
 sleep = 3
 
-def vadim(step, max_iter=1000):
+def vadim(alertness, hypertension, intoxication, time_since_slept, time_elapsed, work_done):
     """A reference strategy based on a certain PhD student named Vadim"""
 
-    while i < max_iter:
-        step(drink_coffee)
-        
-        for work_idx in range(31):
-            step(do_nothing)
+    if time_since_slept == 0:
+        return drink_coffee
+    elif time_since_slept < 31:
+        return just_work
+    else:
+        return sleep
 
-        step(sleep)
+while True:
+    observations = list(map(float, input().strip().split()))
+    print(vadim(*observations))
